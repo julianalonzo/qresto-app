24dac578ca58f21c34a7fdb7efc14865a8edcab3
Julian Aubrey Alonzo <julianaubreya@gmail.com>
2020-01-21 16:32:03 -0700

    Added initial screen for Restaurant

---
+++ screens/Restaurant.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function Restaurant() {
+  return (
+    <View>
+      <Text>Restaurant Screen</Text>
+    </View>
+  );
+}
+
+export default Restaurant;
